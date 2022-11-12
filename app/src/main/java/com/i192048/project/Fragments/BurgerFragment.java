2f258da2ed5a855b9ca8c1037aa79dddc98c766a
HRH@DESKTOP-0JP7NF9 <qaahsan92@gmail.com>
2022-11-13 00:28:21 +0500

    dialog box ready

--- app/src/main/java/com/i192048/project/Fragments/BurgerFragment.java
+++ app/src/main/java/com/i192048/project/Fragments/BurgerFragment.java
@@ -1,16 +1,28 @@
 package com.i192048.project.Fragments;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.i192048.project.R;
 
 public class BurgerFragment extends Fragment {
+
 
     public BurgerFragment() {
         // Required empty public constructor
@@ -19,7 +31,23 @@ public class BurgerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_burger, container, false);
+        View view = inflater.inflate(R.layout.fragment_burger, container, false);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        CardView cardView = view.findViewById(R.id.first_item);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+        return view;
+    }
+
+    private void showDialog(){
+        Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.custom_dialog);
+        dialog.show();
     }
 }
